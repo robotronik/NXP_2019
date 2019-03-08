b58dbc904db145cc56f4aeb4bc6d6224a3e8d989
Trukbidule <46558979+Trukbidule@users.noreply.github.com>
2019-03-08 13:22:49 +0100

    Update RobotronikToolkit.h

--- LMO/code_de_base/RobotronikToolkit.h
+++ LMO/code_de_base/RobotronikToolkit.h
@@ -1,13 +1,47 @@
-#include "CustomCarLink.h"
+#include "mbed.h"
 
-//int temps = 0;
+#define MOTOR_PERIOD (float)(0.020) //sec
 
-CustomCarLink::CustomCarLink(PinName tx, PinName rx, const char *name, int baud):Serial(tx, rx, name, 115200){}
+#define SERVO_PERIOD (float)(0.020) //sec
+#define SERVO_MIN_PULSE_WIDTH (float)0.00100  //sec, min=right limit
+#define SERVO_MAX_PULSE_WIDTH (float)0.00145  //sec, max=left limit
 
-int sendData(){
-    return 0;  
-}
+#define CAMERA_CLK_PERIOD (float)(0.0002)
+#define CAMERA_SI_PERIOD (float)(0.0002) //for the SI signal
 
-int put(void* data, size_t size){
-    return 0;
-}
+/* Pin number:
+    PTA12  Servo 1\n
+    PTA13  Servo 2\n
+    
+    PTB8   (Camera CCD1 SI)\n
+    PTB9   (Camera CCD1 CLK)\n
+    PTC2   (Camera CCD1 A0)\n
+
+    PTA4    Motor1 PWM1 FTM0_CH1\n
+    PTA5    Motor1 PWM2 FTM0_CH2\n
+    PTC8    Motor2 PWM1 FTM0_CH4\n
+    PTC9    Motor2 PWM2 FTM0_CH5\n
+    }
+*/
+
+void initToolkit();
+void updateToolkit();
+
+void setMotorPWM(float motorL, float motorR);
+void setServomotorPWM(float dir);
+void readCamera(int data[]);
+void readCamera2(int data[]);
+
+//void detect(int img[], int &left, int &right);
+void sendRealTimeLog();//send to the PC the data using CustomCarLink
+
+//------------------------------Tests and calibration------------------------------
+void testServo(float maxDir);//sweep
+void calibrateServo();
+void testMotorDC(float max);
+
+void testCamera();
+void calibrateCamera();
+
+void testSerial();
+
