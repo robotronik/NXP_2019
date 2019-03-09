7042a5b1e7fa4780823c6b10f1ddad93aa6bdc5b
Vandekin <nicolas.cubric@grenoble-inp.org>
2019-03-09 14:49:44 +0100

    correction + type de e

--- LMO/function.h
+++ LMO/function.h
@@ -4,7 +4,7 @@ float * ck(float *x, float *y, int sizexy);
 void newton(float *x, float *y, int sizexy);
 void detection(float *x, int sizexy, uint16 *data_trait, int sizedat, uint16 *l, uint16 *r, int sizelr):
 void detect(float * y, int sizexy, uint16 data_trait);
-// correction
+uint16 correction(uint16 data[128], double *data_trait, int *sizedat);
 void val_erreur(unit16 *l, unit16 *r, int sizelr, uint16 b,  unit16 *e, int sizee);
 float regulation_vitesse(float vmax, float vmin, unit16 e);
 float trajectoire(unit16 *e, int sizee);
